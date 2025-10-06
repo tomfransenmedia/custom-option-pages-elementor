4c2a959f9308598f2c3e26e5d8ca6ee49a7b4ab7
Mac <mac@Toms-MacBook-Pro.local>
2025-10-06 15:37:58 +0300

    Update plugin version to 1.0.5

--- custom-option-pages-elementor.php
+++ custom-option-pages-elementor.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Option Pages for Elementor
 Plugin URI: https://tomfransen.nl
 Description: Implements custom option pages / reusable content via ACF and exposes them as dynamic tags in Elementor Pro.
-Version: 1.0.4
+Version: 1.0.5
 Author: TomFransen Media
 Author URI: https://tomfransen.nl
 Text Domain: custom-option-pages-elementor
